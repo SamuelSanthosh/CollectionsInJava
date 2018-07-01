6021b11b4cee21f9bcbee09b4d00a76d565cbc8a
SamuelSanthosh <35218854+SamuelSanthosh@users.noreply.github.com>
2018-07-01 12:36:37 +0530

    Create ArrayListNCopies.java

---
+++ ArrayListNCopies.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        ArrayList <Integer> list = new ArrayList<Integer>(Collections.nCopies(5,10));
+        System.out.println(list);
+    }
+}
