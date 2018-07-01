3d5de8aea22d2e9b0fba3d015521867495e6242f
SamuelSanthosh <35218854+SamuelSanthosh@users.noreply.github.com>
2018-07-01 19:50:02 +0530

    Create ArrayListClear.java

---
+++ ArrayListClear.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        ArrayList <Object> list1 = new ArrayList<Object>()
+        {{
+            add(1);
+            add(34);
+            add(23);
+            add(76);
+            add(-365);
+            add(1);
+            add(88);
+            add("aaaa");
+            add(5.99);
+            add(767.5);
+        }};
+        System.out.println(list1);
+        
+        list1.clear();
+        list1.removeAll(list1);
+        System.out.println(list1);
+    }
+}
